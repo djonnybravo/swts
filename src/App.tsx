95059c70230dd766ad7a273591d5962013a97e69
djonnybravo <djonnybravo29@gmail.com>
2023-05-04 17:11:13 +0300

    add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header>
+                <img/>
+
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+            </nav>
+            <main>
+                <div>Main Content</div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
