bbf57c84351d0b32bfc7014e40d354c581ff4f69
djonnybravo <djonnybravo29@gmail.com>
2023-05-07 15:10:26 +0300

    rollback and fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Nav/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Route} from "react-router-dom";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <div className={"content"}>
                  {/*<Profile />*/}
                 <Dialogs/>
+                <Route component={Dialogs}></Route>
             </div>
         </div>
     );
