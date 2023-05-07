475527cf71745c93c4345217baf08fca94b870ee
djonnybravo <djonnybravo29@gmail.com>
2023-05-07 15:13:31 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Nav/Navbar";
-import Profile from "./components/Profile/Profile";
+
 import Dialogs from "./components/Dialogs/Dialogs";
-import {Route} from "react-router-dom";
+
 
 function App() {
     return (
@@ -14,7 +14,7 @@ function App() {
             <div className={"content"}>
                  {/*<Profile />*/}
                 <Dialogs/>
-                <Route component={Dialogs}></Route>
+
             </div>
         </div>
     );
