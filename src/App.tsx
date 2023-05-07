89d50220a9e67533ae7e687c4ec2eb4a1a62c099
djonnybravo <djonnybravo29@gmail.com>
2023-05-07 15:23:40 +0300

    another fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Nav/Navbar";
+import Profile from "./components/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {Route} from "react-router-dom";
-import Profile from './components/Profile/Profile';
 
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className={"content"}>
-
-                <Route component={Profile}></Route>
-                <Route component={Dialogs}></Route>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className={"content"}>
+                    <Route component={Profile}></Route>
+                    <Route component={Dialogs}></Route>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
