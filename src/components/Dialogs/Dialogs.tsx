4a43845b197f50e30285639ca8798082cfc65ca6
djonnybravo <djonnybravo29@gmail.com>
2023-05-08 19:27:46 +0300

    add css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import classes from "./Dialogs.module.css";
+import {callbackify} from "util";
 
 const Dialogs = () => {
     return (
-        <div className={classes.contents}>
-            Dialogs
+        <div className={classes.dialogsPage}>
+            <div className={classes.dialogs__items}>
+                <div className={classes.dialog}> Dimych</div>
+                <div className={classes.dialog}> Petya</div>
+                <div className={classes.dialog + " " + classes.active}> Djon</div>
+                <div className={classes.dialog}> Ivan</div>
+                <div className={classes.dialog}> UserN</div>
+            </div>
+            <div className={classes.dialog__messages}>
+                <div className={classes.message}>Hellloooo</div>
+                <div className={classes.message}>Hellloooo</div>
+            </div>
         </div>
     );
 };
