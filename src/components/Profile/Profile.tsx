3d141fecd859a7844690f37c41bc3262509269f1
djonnybravo <djonnybravo29@gmail.com>
2023-05-08 20:29:52 +0300

    add ProfileInfo.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import classes from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div className={classes.ProfilePage}>
-            <div>
-                <img src="https://telecomdom.com/wp-content/uploads/2019/12/shapka_for_youtube-fon-6-2048x1152.jpg"
-                     alt="" width="1440px" height="200px"/>
-            </div>
-            <div>
-                <div>
-                    <img
-                        src="https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg"
-                        alt="" width="100px" height="100px"/>
-                </div>
-                <div>
-
-                </div>
-            </div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     );
 };
+
+
+
 
 export default Profile;
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://telecomdom.com/wp-content/uploads/2019/12/shapka_for_youtube-fon-6-2048x1152.jpg"
+                     alt="" width="1440px" height="200px"/>
+            </div>
+            <div>
+                <div>
+                    <img
+                        src="https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg"
+                        alt="" width="100px" height="100px"/>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileInfo;
