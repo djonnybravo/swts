899f88c04a8e17f89f58e79022075c22c2840bb4
djonnybravo <djonnybravo29@gmail.com>
2023-05-08 18:53:00 +0300

    add links for rest pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Navbar from "./components/Nav/Navbar";
 import Profile from "./components/Profile/Profile";
 import {BrowserRouter, Route} from "react-router-dom";
 import Dialogs from "./components/Dialogs/Dialogs";
+import MusicPage from "./components/Music/MusicPage";
+import NewsPage from "./components/News/NewsPage";
+import SettingsPage from "./components/Settings/SettingsPage";
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
                 <div className={"content"}>
                     <Route path={"/profile"} component={Profile}></Route>
                     <Route path={"/dialogs"} component={Dialogs}></Route>
+                    <Route path={"/music"} component={MusicPage}></Route>
+                    <Route path={"/news"} component={NewsPage}></Route>
+                    <Route path={"/settings"} component={SettingsPage}></Route>
                 </div>
             </div>
         </BrowserRouter>
