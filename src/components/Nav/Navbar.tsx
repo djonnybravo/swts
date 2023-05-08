3245799459db1fcd39e436677a94b97479c6fc21
djonnybravo <djonnybravo29@gmail.com>
2023-05-08 18:54:58 +0300

    add links for rest pages in navbar

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import classes from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href={"./profile"}>Profile</a>
+                <NavLink to={"/profile"}>Profile</NavLink>
             </div>
             <div className={classes.item + classes.active}>
-                <a href={"./dialogs"}>Messages</a>
+                <NavLink to={"/dialogs"}>Messages</NavLink>
             </div >
             <div className={classes.item}>
-                <a>News</a>
+                <NavLink to={"/news"}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a>Music</a>
+                <NavLink to={"/music"}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a>Settings</a>
+                <NavLink to={"/settings"}>Settings</NavLink>
             </div>
         </nav>
     );
