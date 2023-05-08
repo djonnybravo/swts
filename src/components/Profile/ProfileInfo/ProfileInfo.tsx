343c4068681006db6529973eeb47e31f54fba021
djonnybravo <djonnybravo29@gmail.com>
2023-05-08 20:33:23 +0300

    fix

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from "./ProfileInfo.module.css"
 
 const ProfileInfo = () => {
     return (
-        <div>
+        <div className={classes.profile}>
             <div>
                 <img src="https://telecomdom.com/wp-content/uploads/2019/12/shapka_for_youtube-fon-6-2048x1152.jpg"
                      alt="" width="1440px" height="200px"/>
@@ -12,10 +13,9 @@ const ProfileInfo = () => {
                     <img
                         src="https://omoro.ru/wp-content/uploads/2018/05/prikilnie-kartinki-na-avatarky-dlia-devyshek-12.jpg"
                         alt="" width="100px" height="100px"/>
+                    description
                 </div>
-                <div>
 
-                </div>
             </div>
         </div>
     );
