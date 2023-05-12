0c4443ac85aa9bf608421d3077b555c87d140893
djonnybravo <djonnybravo29@gmail.com>
2023-05-12 19:26:40 +0300

    fix dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,21 +3,10 @@ import classes from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import dialogItem from "./DialogItem/DialogItem";
+import {DialogsPageType} from "../../redux/state";
 
-type DialogsPropsType = {
-    dialogs: dialogType[]
-    messages: messageType[]
-}
-type dialogType = {
-    id: number
-    name: string
-}
 
-type messageType = {
-    id: number
-    messageText: string
-}
-const Dialogs = (props:DialogsPropsType) => {
+const Dialogs = (props:DialogsPageType) => {
 
 
 
@@ -29,7 +18,7 @@ const Dialogs = (props:DialogsPropsType) => {
 
             </div>
             <div className={classes.dialog__messages}>
-                {props.messages.map(message => <Message messageText={message.messageText}/>)}
+                {props.messages.map(message => <Message id={message.id} message={message.message}/>)}
             </div>
 
         </div>
