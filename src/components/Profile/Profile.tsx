9a53ec1da7b5a74fd352a18e11fa9c2a7dcb8721
djonnybravo <djonnybravo29@gmail.com>
2023-05-15 17:29:48 +0300

    feat: addNewPost

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import classes from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {ProfilePageType} from "../../redux/state";
+import {addPost, PostsType, ProfilePageType} from "../../redux/state";
 
 
+export type ProfilePropsType = {
+    posts: PostsType[]
+    addPost: (newPost: string) => void
+}
 
-const Profile = (props: ProfilePageType) => {
+
+const Profile = (props: ProfilePropsType) => {
     return (
         <div className={classes.ProfilePage}>
             <ProfileInfo/>
-            <MyPosts posts={props.posts}/>
+            <MyPosts posts={props.posts} addPost={addPost}/>
         </div>
     );
 };
