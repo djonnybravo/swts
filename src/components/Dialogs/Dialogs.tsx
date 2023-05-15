ac66f1102fd5eea64b90ceac956fe3e69d63e3a2
djonnybravo <djonnybravo29@gmail.com>
2023-05-15 13:58:53 +0300

    create callback functions for addMessage

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -8,8 +8,10 @@ import {DialogsPageType} from "../../redux/state";
 
 const Dialogs = (props:DialogsPageType) => {
 
+    let newMessageElement = React.createRef<HTMLTextAreaElement>()
+    let addMessage = () => {
 
-
+    }
     return (
         <div className={classes.dialogsPage}>
             <div className={classes.dialogs__items }>
@@ -20,7 +22,10 @@ const Dialogs = (props:DialogsPageType) => {
             <div className={classes.dialog__messages}>
                 {props.messages.map(message => <Message id={message.id} message={message.message}/>)}
             </div>
-
+            <div>
+                <textarea ref={newMessageElement}></textarea>
+                <button onClick={addMessage}>send</button>
+            </div>
         </div>
     );
 };
