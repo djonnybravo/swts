8961f959e6de399c1e745f399a0b1878ebe54220
djonnybravo <djonnybravo29@gmail.com>
2023-05-21 16:27:23 +0300

    fix: Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {addPost, PostsType, ProfilePageType, updateNewPostText} from "../../redux/state";
+import { PostsType} from "../../redux/state";
 
 
 export type ProfilePropsType = {
@@ -17,7 +17,7 @@ const Profile = (props: ProfilePropsType) => {
     return (
         <div className={classes.ProfilePage}>
             <ProfileInfo/>
-            <MyPosts posts={props.posts} addPost={addPost} newPostText={props.newPostText} updateNewPostText={props.updateNewPostText}/>
+            <MyPosts posts={props.posts} addPost={props.addPost} newPostText={props.newPostText} updateNewPostText={props.updateNewPostText}/>
         </div>
     );
 };
